5cb50d2ea9bbe36009510dc0641158debed5992d
奇风 <qifeng.whl@antgroup.com>
2021-03-22 21:00:02 +0800

    feat: 优化性能

--- cache.go
+++ cache.go
@@ -2,6 +2,7 @@ package lfucache
 
 type Cache interface {
 	Get(key string) (val interface{}, has bool)
+	OptGet(key string) (val interface{}, has bool)
 	Set(key string, value interface{})
 	Del(key string)
 	Clear() int
